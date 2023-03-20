999ef1104adf4b9a386dd3f1cca69d12673b135d
kleves Henrique <kleveshenrique@gmail.com>
2023-03-19 22:39:55 -0300

    deploy teste

--- src/services/api.ts
+++ src/services/api.ts
@@ -15,15 +15,15 @@ export const getPizzas = async () =>await api.get<SnackData[]>('/pizzas')
 export const getDrinks = async () =>await api.get<SnackData[]>('/drinks')
 export const getIceCreams = async () =>await api.get<SnackData[]>('/ice-creams')
 
-export function getSnacks(snackName:string){
-    axios.get(`${process.env.REACT_APP_API_URL}/snacks/${snackName}`)
-    .then((response)=>{
-      return response.data
-    })
-    .catch((err)=>{
-      console.log(err)
-      return err
-    })
-}
+// export function getSnacks(snackName:string){
+//     axios.get(`${process.env.REACT_APP_API_URL}/snacks/${snackName}`)
+//     .then((response)=>{
+//       return response.data
+//     })
+//     .catch((err)=>{
+//       console.log(err)
+//       return err
+//     })
+// }
 
 export default api
